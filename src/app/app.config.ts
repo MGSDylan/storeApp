f40f630a51767bdfdc0484a325197d3116c0600b
MGSDylan <dylan.caceres.26@hotmail.com>
2024-12-07 19:42:45 -0500

    congif-prefetching

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes,withComponentInputBinding()), provideClientHydration(),
+  providers: [provideRouter(routes,withComponentInputBinding(),withPreloading(PreloadAllModules)), provideClientHydration(),
     provideHttpClient()]
 };//withComponentInputBinding() llega los parametros como input a las paginas
+//,withPreloading(PreloadAllModules) precarga los demas archivos js que aun no se an visitado
